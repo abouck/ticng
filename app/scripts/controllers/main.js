405a6f2b3501f7beb300ccc311726c23b008a100
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-10-03 17:14:51 -0700

    Regular JS works with draw, ready for transition to angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,158 +1,120 @@
-// var turn
-// var winArr = []
-// // function start()
+var turn
+var winArr = []
+var alreadyWon = false
+// function start()
 
-
-// function pgLoad(){
+function pgLoad(){
 	
-// 	var elems = document.getElementsByClassName('box');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].style.opacity = 0;
-// }
-//     var elems = document.getElementsByClassName('choice');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].setAttribute('onclick',"start()");
-// }
+	var elems = document.getElementsByClassName('box');
+	for(var i = 0; i < elems.length; i++) {
+    elems[i].style.opacity = 0;
+}
+    var elems = document.getElementsByClassName('choice');
+	for(var i = 0; i < elems.length; i++) {
+    elems[i].setAttribute('onclick',"start()");
+}
 
-// }
-// /*
-// // Attempt at fade
-// function timeout()
-// {
-// 	var startOp = 1.0
-// var start =	document.getElementById('start')
-// start.style.opacity = 
-// 1 - (1 * Math.cos(startOp));
-// startOp -= 0.05; // Small small increments
-// if(startOp <= 0)
-// {
-// setTimeout("timeout();", 10);
-// }
-// }
-// */
-// // sets background image (X or O depending in turn)
-// function setBg(){
+}
 
-// document.getElementById("gamebox").setAttribute('style',"background-image:url(img/"+turn+".png)")
-// }
+// sets background image (X or O depending in turn)
+function setBg(){
 
-// // Starts the game
-// function start(){
-// 	turn = event.target.innerHTML
-// 	document.getElementById('start').style.top = "-100000px"
+document.getElementById("gamebox").setAttribute('style',"background-image:url(img/"+turn+".png)")
+}
+
+// Starts the game
+function start(){
+	turn = event.target.innerHTML
+	document.getElementById('start').style.top = "-100000px"
 	
-// 	var elems = document.getElementsByClassName('box');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].style.opacity = 1;
-//     elems[i].innerHTML = ""
-//     elems[i].setAttribute('onclick',"fillBox()");
-// 	}	
-// 	document.getElementById('win').style.display = "none"
-// 	setBg()
-// }
+	var elems = document.getElementsByClassName('box');
+	for(var i = 0; i < elems.length; i++) {
+    elems[i].style.opacity = 1;
+    elems[i].innerHTML = ""
+    elems[i].setAttribute('onclick',"fillBox()");
+	}	
+	document.getElementById('win').style.display = "none"
+	setBg()
+}
 
-// // Runs on win,
-// function win(){
-// 	var elems = document.getElementsByClassName('box');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].setAttribute('onclick',"");
-// 	}
-// 	document.getElementById('win').style.display = "block"
-// 	document.getElementById('win').innerHTML = turn+' Wins!!<div class="choice chooseX" onclick="start()">X</div><div class="choice chooseO" onclick="start()">O</div>'
-// }
-
-
-
-
-// function fillBox()	{
-
-// // Determines if box is blank or not
-// 	if(event.target.innerHTML == ''){
+// Runs on win,
+function win(x,y){
 	
-// 	event.target.innerHTML = turn
+	var elems = document.getElementsByClassName('box');
+	for(var i = 0; i < elems.length; i++) {
+    elems[i].setAttribute('onclick',"");
+	}
+	document.getElementById('win').style.display = "block"
+	document.getElementById('win').innerHTML = x+y+'<div class="choice chooseX" onclick="start()">X</div><div class="choice chooseO" onclick="start()">O</div>'
+	alreadyWon = true
+}
+
+
+
+
+function fillBox()	{
+
+// Determines if box is blank or not
+	if(event.target.innerHTML == ''){
 	
-// 	}
-
-// 	else
-// 		alert('Choose an empty box!')
-
-// // Fills winArray for win eval
-// 	for(i=1; i<=9; ++i)
-// 	{
-// 		winArr.push(
-// 			document.getElementById("b"+i).innerHTML
-// 			);
-// 	}
+	event.target.innerHTML = turn
 	
-// // Win Condition logic
+	}
 
-// // Rows
-// 	for(w=0; w<=6; w+=3)
-// 	{
-// 		if((winArr[w] != '') && (winArr[w] == winArr[w+1]) && (winArr[w+1] == winArr[w+2])){
-// 		win();
-// 		}
-// 	}
+	else
+		alert('Choose an empty box!')
 
-// // Columns
-// 	for(w=0; w<=2; ++w){
-// 		if((winArr[w] != '') && (winArr[w] == winArr[w+3]) && (winArr[w+3] == winArr[w+6])){
-// 		win();
-// 		}
-// 	}
+// Fills winArray for win eval
+	for(i=1; i<=9; ++i)
+	{
+		winArr.push(
+			document.getElementById("b"+i).innerHTML
+			);
+	}
+	
+// Win Condition logic
 
-// // Diagonals
-// 	if((winArr[0] != '') && (winArr[0] == winArr[4]) && (winArr[4] == winArr[8])) {
-// 	win()
-// 	}
+// Rows
+	for(w=0; w<=6; w+=3)
+	{
+		if((winArr[w] != '') && (winArr[w] == winArr[w+1]) && (winArr[w+1] == winArr[w+2])){
+		win(turn," Wins!!");
+		}
+	}
 
-// 	else if((winArr[2] != '') && (winArr[2] == winArr[4]) && (winArr[4] == winArr[6])) {
-// 	win()
-// 	}
+// Columns
+	for(w=0; w<=2; ++w){
+		if((winArr[w] != '') && (winArr[w] == winArr[w+3]) && (winArr[w+3] == winArr[w+6])){
+		win(turn," Wins!!");
+		}
+	}
 
+// Diagonals
+	if((winArr[0] != '') && (winArr[0] == winArr[4]) && (winArr[4] == winArr[8])) {
+	win(turn," Wins!!")
+	}
 
-// /*	if((winArr[0] != '') && (winArr[0] == winArr[1]) && (winArr[1] == winArr[2])){
-// 	alert(turn + " Wins!!")
-// 	}
+	else if((winArr[2] != '') && (winArr[2] == winArr[4]) && (winArr[4] == winArr[6])) {
+	win(turn," Wins!!")
+	}
+// Draw (won't let you win on last turn)
+	var draw = 0
+	for (var i =0; i <winArr.length; i++) {
+		if(winArr[i] !="")
+			draw++
+			};
+		if(draw == 9 && alreadyWon == false)
+			win("Draw!","")
+// Switches turn to other side
+	if(turn == 'X')
+		turn = 'O'
+	else
+		turn = 'X'
 
-// 	else if((winArr[3] != '') && (winArr[3] == winArr[4]) && (winArr[4] == winArr[5])) {
-// 	alert(turn + " Wins!!")
-// 	}
+// Changes background image turn indicator
 
-// 	else if((winArr[6] != '') && (winArr[6] == winArr[7]) && (winArr[7] == winArr[8])) {
-// 	alert(turn + " Wins!!")
-// 	}
-
-// 	else if((winArr[0] != '') && (winArr[0] == winArr[3]) && (winArr[3] == winArr[6])) {
-// 	alert(turn + " Wins!!")
-// 	}
-
-// 	else if((winArr[1] != '') && (winArr[1] == winArr[4]) && (winArr[4] == winArr[7])) {
-// 	alert(turn + " Wins!!")
-// 	}
-
-// 	else if((winArr[2] != '') && (winArr[2] == winArr[5]) && (winArr[5] == winArr[8])) {
-// 	alert(turn + " Wins!!")
-// 	}
-
-// 	else if((winArr[0] != '') && (winArr[0] == winArr[4]) && (winArr[4] == winArr[8])) {
-// 	alert(turn + " Wins!!")
-// 	}
-
-// 	else if((winArr[2] != '') && (winArr[2] == winArr[4]) && (winArr[4] == winArr[6])) {
-// 	alert(turn + " Wins!!")
-// 	}
-// */
-// // Switches turn to other side
-// 	if(turn == 'X')
-// 		turn = 'O'
-// 	else
-// 		turn = 'X'
-
-// // Changes background image turn indicator
-
-// 	setBg()
-// //	Clears winArr for win logic	
-// 	winArr = []
-// }
+	setBg()
+//	Clears winArr for win logic	
+	winArr = []
+}
 
