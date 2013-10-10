f6372d6506787d9168bbfbd3d1fdd70be045ec48
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-10-10 12:30:32 -0700

    All win conditions functional, ready for FireBase.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,6 @@ angular.module('newTicApp')
 	[{value: '', r: 3, c: 1},{value: '', r: 3, c: 2},{value: '', r: 3, c: 3}]]
 
 	var playerTurn = 1;
-
 	$scope.fill = function(sqr,row) {
 		if(sqr.value == "X" || sqr.value == "O")
 		alert("Please Choose an Empty Square!")
@@ -26,35 +25,13 @@ angular.module('newTicApp')
 
 $scope.win = function(row,sqr){		
 	var winTest = 0
+	var len = this.ticArr.length
 // Win eval for rows
-		for (var i=0; i<this.ticArr.length; i++) {
+		for (var i=0; i<len; i++) {
 			winTest = 0
-			for (var o=0; o<this.ticArr.length; o++) {
-				
+			for (var o=0; o<len; o++) {
 				switch(this.ticArr[i][o].value)
 				{
-
-					case "X":
-						++winTest
-						break;
-					case "O":
-						--winTest
-						break;
-				}
-			};
-			if(Math.abs(winTest) == 3){
-		$scope.gameWon = true
-	}
-		}
-		
-// Win eval for columns
-		for (var i=0; i<this.ticArr.length; i++) {
-			winTest = 0
-			for (var o=0; o<this.ticArr.length; o++) {
-				
-				switch(this.ticArr[o][i].value)
-				{
-
 					case "X":
 						++winTest
 						break;
@@ -64,136 +41,83 @@ $scope.win = function(row,sqr){
 				}
 			};
 			if(Math.abs(winTest) == 3)
-		alert(sqr.value+" won on column " + (i+1))
+		$scope.gameWon = true
+		}
+		
+// Win eval for columns
+		for (var i=0; i<len; i++) {
+			winTest = 0
+			for (var o=0; o<len; o++) {
+				switch(this.ticArr[o][i].value)
+				{
+					case "X":
+						++winTest
+						break;
+					case "O":
+						--winTest
+						break;
+				}
+			};
+			if(Math.abs(winTest) == 3) 
+			$scope.gameWon = true
+
 		};
-}
+	// Diagonals eval
+	var diagTest1 = 0;
+	var diagTest2 = 0;
+	for(var i = 0;i < len ; ++ i)
+	{
+		// Diagonals going from upper-left to lower-right
+		switch(this.ticArr[i][i].value)
+			{
+				case "X":
+					++diagTest1;
+					break;
+				case "O":
+					--diagTest1;
+					break;
+			}
+		// Diagonals going from upper-right to lower-left
+		switch(this.ticArr[i][(len-1)-i].value)
+			{
+				case "X":
+					++diagTest2;
+					break;
+				case "O":
+					--diagTest2;
+					break;
+			}
+			
+		if(diagTest1 == len) {
+			alert("Dude X won on diagonal \\");
+		$scope.gameWon = true}
+		if(diagTest1 == -len){
+			alert("Dude O won on diagonal \\");
+			$scope.gameWon = true}
+		if(diagTest2 == len) {
+			alert("Dude X won on diagonal /");
+			$scope.gameWon = true}
+		if(diagTest2 == -len) {
+			alert("Dude O won on diagonal /");
+			$scope.gameWon = true}
 
-
+	}
+	// Cats game eval
+	$scope.cats = false
+	var catsTest=0
+	for (var i=0; i<len; i++) {
+			winTest = 0
+			for (var o=0; o<len; o++) {
+				if(this.ticArr[i][o].value != "")
+						++catsTest		
+				};
+	}
+	if(catsTest == 9 && $scope.gameWon != true) {
+	$scope.cats = true
+	}
+	}	
 	});
 	
 
 
-
-
-
-// var turn
-// var winArr = []
-// var alreadyWon = false
-// // function start()
-
-// function pgLoad(){
-	
-// 	var elems = document.getElementsByClassName('box');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].style.opacity = 0;
-// }
-//     var elems = document.getElementsByClassName('choice');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].setAttribute('onclick',"start()");
-// }
-
-// }
-
-// // sets background image (X or O depending in turn)
-// function setBg(){
-
-// document.getElementById("gamebox").setAttribute('style',"background-image:url(img/"+turn+".png)")
-// }
-
-// // Starts the game
-// function start(){
-// 	turn = event.target.innerHTML
-// 	document.getElementById('start').style.top = "-100000px"
-	
-// 	var elems = document.getElementsByClassName('box');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].style.opacity = 1;
-//     elems[i].innerHTML = ""
-//     elems[i].setAttribute('onclick',"fillBox()");
-// 	}	
-// 	document.getElementById('win').style.display = "none"
-// 	setBg()
-// 	alreadyWon = false
-// }
-
-// // Runs on win,
-// function win(x,y){
-	
-// 	var elems = document.getElementsByClassName('box');
-// 	for(var i = 0; i < elems.length; i++) {
-//     elems[i].setAttribute('onclick',"");
-// 	}
-// 	document.getElementById('win').style.display = "block"
-// 	document.getElementById('win').innerHTML = x+y+'<div class="choice chooseX" onclick="start()">X</div><div class="choice chooseO" onclick="start()">O</div>'
-// 	alreadyWon = true
-// }
-
-
-
-
-// function fillBox()	{
-
-// // Determines if box is blank or not
-// 	if(event.target.innerHTML == ''){
-	
-// 	event.target.innerHTML = turn
-	
-// 	}
-
-// 	else
-// 		alert('Choose an empty box!')
-
-// // Fills winArray for win eval
-// 	for(i=1; i<=9; ++i)
-// 	{
-// 		winArr.push(
-// 			document.getElementById("b"+i).innerHTML
-// 			);
-// 	}
-	
-// // Win Condition logic
-
-// // Rows
-// 	for(w=0; w<=6; w+=3)
-// 	{
-// 		if((winArr[w] != '') && (winArr[w] == winArr[w+1]) && (winArr[w+1] == winArr[w+2])){
-// 		win(turn," Wins!!");
-// 		}
-// 	}
-
-// // Columns
-// 	for(w=0; w<=2; ++w){
-// 		if((winArr[w] != '') && (winArr[w] == winArr[w+3]) && (winArr[w+3] == winArr[w+6])){
-// 		win(turn," Wins!!");
-// 		}
-// 	}
-
-// // Diagonals
-// 	if((winArr[0] != '') && (winArr[0] == winArr[4]) && (winArr[4] == winArr[8])) {
-// 	win(turn," Wins!!")
-// 	}
-
-// 	else if((winArr[2] != '') && (winArr[2] == winArr[4]) && (winArr[4] == winArr[6])) {
-// 	win(turn," Wins!!")
-// 	}
-// // Draw (won't let you win on last turn)
-// 	var draw = 0
-// 	for (var i =0; i <winArr.length; i++) {
-// 		if(winArr[i] !="")
-// 			draw++
-// 			};
-// 		if(draw == 9 && alreadyWon == false)
-// 			win("Draw!","")
-// // Switches turn to other side
-// 	if(turn == 'X')
-// 		turn = 'O'
-// 	else
-// 		turn = 'X'
-
-// // Changes background image turn indicator
-
-// 	setBg()
-// //	Clears winArr for win logic	
-// 	winArr = []
-// }
 
